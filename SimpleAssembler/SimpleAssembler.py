41d19ae931261c5a328c6f0b83652212b2e7d9c4
Manav Mittal <65654441+manavmittal05@users.noreply.github.com>
2022-07-06 17:58:42 +0530

    Update SimpleAssembler.py

--- SimpleAssembler/SimpleAssembler.py
+++ SimpleAssembler/SimpleAssembler.py
@@ -98,3 +98,169 @@ def F(opcode):
     a = list(str(getcode(opcode)))
     a = a + ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
     print(''.join(a))
+
+
+cmd = sys.stdin.read().split('\n')
+cmd = [x.split() for x in cmd if x != '\n' and x != '']
+if len(cmd) > 256:
+    print("Compilation Error: Memory Overflow!")
+    exit()
+ops_A = ['add', 'sub', 'mul', 'xor', 'or', 'and']
+ops_B = ['mov', 'ls', 'rs']
+ops_C = ['mov', 'div', 'not', 'cmp']
+ops_D = ['ld', 'st']
+ops_E = ['jmp', 'jlt', 'jgt', 'je']
+ops_F = ['hlt']
+regs = ['R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R0']
+variables = {}
+var_count = 0
+labels = {}
+no_cmds = len(cmd)
+e_count = 0
+
+if not cmd:
+    exit()
+
+for w in range(len(cmd)):
+    i = cmd[w]
+    if ':' in i[0]:
+        labels[i[0][:-1]] = w
+        i.pop(0)
+
+if ['hlt'] not in cmd:
+    e_count += 1
+    print('Syntax Error: hlt instruction is not present in the file.')
+
+if cmd[-1] != ['hlt'] and ['hlt'] in cmd:
+    e_count += 1
+    print('Syntax Error: hlt instruction is not found at the EOF')
+
+if cmd.count(['hlt']) > 1:
+    e_count += 1
+    print('Syntax Error: There are more than 1 hlt instructions in the file.')
+
+while cmd != [] and cmd[0][0] == 'var':
+    variables[cmd[0][1]] = var_count
+    cmd.pop(0)
+    var_count += 1
+
+for i in variables.keys():
+    variables[i] += no_cmds-var_count
+
+for w in range(len(cmd)):
+    i = cmd[w]
+    if 'var' in i:
+        e_count += 1
+        print('Syntax Error: Invalid variable declaration in line %d' % (w + var_count + 1))
+        continue
+    if ':' in ''.join(i):
+        e_count += 1
+        print('Syntax Error: Invalid label declaration in line %d' % (w + var_count + 1))
+        continue
+    if 'FLAGS' in i:
+        if i[0] != 'mov':
+            e_count += 1
+            print('Syntax Error: Invalid use of Flag registers in line %d' % (w + var_count + 1))
+            continue
+        if i[2] != 'FLAGS':
+            e_count += 1
+            print('Syntax Error: Invalid use of Flag registers in line %d' % (w + var_count + 1))
+            continue
+
+    try:
+        if i[0] not in ops_A + ops_B + ops_C + ops_D + ops_E + ops_F:
+            e_count += 1
+            print('Syntax Error: Invalid instruction in line %d' % (w + var_count + 1))
+            continue
+    except IndexError:
+        e_count += 1
+        print('Syntax Error: Incorrect use of syntax in line %d' % (w + var_count + 1))
+        continue
+
+    if i[0] == 'mov':
+        try:
+            if i[1] not in regs:
+                e_count += 1
+                print('Syntax Error: Invalid use of registers in line %d' % (w + var_count + 1))
+                continue
+            if '$' in i[2] and '.' in i[2]:
+                e_count += 1
+                print('Syntax Error: Invalid literal value in line %d' % (w + var_count + 1))
+                continue
+            if '$' in i[2] and (0 > int(i[2][1:]) or int(i[2][1:]) > 255):
+                e_count += 1
+                print('Syntax Error: Invalid literal value in line %d' % (w + var_count + 1))
+                continue
+            if '$' not in i[2] and i[2] not in (regs + ['FLAGS']):
+                e_count += 1
+                print('Syntax Error: Invalid use of registers in line %d' % (w + var_count + 1))
+                continue
+        except IndexError:
+            e_count += 1
+            print("Syntax Error: Incorrect use of syntax at line %d" % (w + var_count + 1))
+            continue
+
+    else:
+        if i[0] in ops_A:
+            try:
+                if i[1] not in regs or i[2] not in regs or i[3] not in regs:
+                    e_count += 1
+                    print('Syntax Error: Invalid use of registers in line %d' % (w + var_count + 1))
+                    continue
+            except IndexError:
+                print('General Syntax Error: Invalid use of syntax in line %d' % (w + var_count + 1))
+                continue
+        if i[0] in ops_C:
+            if i[1] not in regs or i[2] not in regs:
+                e_count += 1
+                print('Syntax Error: Invalid use of registers in line %d' % (w + var_count + 1))
+                continue
+
+        if i[0] in ops_B:
+            try:
+                if i[1] not in regs:
+                    e_count += 1
+                    print('Syntax Error: Invalid use of registers in line %d' % (w + var_count + 1))
+                    continue
+                if '$' in i[2] and '.' in i[2]:
+                    e_count += 1
+                    print('Syntax Error: Invalid literal value in line %d' % (w + var_count + 1))
+                    continue
+                if '$' in i[2] and (0 > int(i[2][1:]) or int(i[2][1:]) > 255):
+                    e_count += 1
+                    print('Syntax Error: Invalid literal value in line %d' % (w + var_count + 1))
+                    continue
+                if '$' not in i[2]:
+                    e_count += 1
+                    print("Syntax Error: '$' sign must be used before a literal value in line %d" % (w + var_count + 1))
+                    continue
+            except IndexError:
+                e_count += 1
+                print("Syntax Error: Incorrect use of syntax at line %d" % (w + var_count + 1))
+                continue
+    if i[0] in ops_D:
+        try:
+            if i[1] not in regs:
+                e_count += 1
+                print('Syntax Error: Invalid use of registers in line %d' % (w + var_count + 1))
+                continue
+            if i[2] not in variables:
+                e_count += 1
+                print("Syntax Error: Use of undefined variable '%s' in line %d" % (i[2], (w + var_count + 1)))
+                continue
+        except IndexError:
+            e_count += 1
+            print("Syntax Error: Incorrect use of syntax at line %d" % (w + var_count + 1))
+            continue
+    if i[0] in ops_E:
+        try:
+            if i[1] not in labels:
+                e_count += 1
+                print("Syntax Error: Use of undefined label '%s' in line %d" % (i[1], (w + var_count + 1)))
+                continue
+        except IndexError:
+            e_count += 1
+            print("Syntax Error: Incorrect use of syntax at line %d" % (w + var_count + 1))
+            continue
+
+
